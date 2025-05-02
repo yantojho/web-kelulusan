d6ba9b666f2e713c906aa2f47e5c7ebdd5d282dc
yantojho <yantojho@gmail.com>
2025-05-02 17:01:30 +0700

    update asset admin dashboard

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Kurikulum - SMKN1-SEPA</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="{{asset('admin/vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/vendors/css/vendor.bundle.base.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/vendors/css/vendor.bundle.addons.css')}}">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{asset('admin/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('admin/css/font-awesome.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('admin/css/select2.css')}}">
+  <link rel="stylesheet" href="{{ asset('admin/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+  @section('css')
+
+  @show
+  <!-- endinject -->
+  <link rel="shortcut icon" href="{{asset('images/LOGO_SMK.png')}}" />
+</head>
+<body>
+    <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+      <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+        <a class="navbar-brand brand-logo" href="index.html" style="color: #2d2d2d">
+          Kurikulum-SEPA
+        </a>
+           <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+          <span class="icon-menu"></span>
+          <i class="fa fa-align-justify" style="color: #fff;"></i>
+        </button>
+      </div>
+
+      <div class="navbar-menu-wrapper d-flex align-items-center">
+        <ul class="navbar-nav navbar-nav-right">
+         
+          <li class="nav-item dropdown d-xl-inline-block">
+            <a class="nav-link dropdown-toggle" id="UserDropdown" href="#" data-toggle="dropdown" aria-expanded="false">
+              <span class="profile-text">Hello, @auth {{Auth::user()->name}} @endauth !</span>
+                @if(isset(Auth::user()->gambar) == 0)
+                  <img class="img-xs rounded-circle"  src="{{asset('images/user/default.png')}}" alt="profile image">
+                @else
+                <img class="img-xs rounded-circle"  src="{{asset('images/user/'.Auth::user()->gambar)}}" alt="profile image">
+                @endif
+            </a>
+            <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="UserDropdown">
+              <a class="dropdown-item p-0">
+                <div class="d-flex border-bottom">
+                 
+                </div>
+              </a>
+              @auth
+              <a class="dropdown-item" style="margin-top: 20px;" href="{{route('user.edit', Auth::user()->id)}}">
+               Edit Profile
+              </a>
+              <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                 Sign Out
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+              </a>
+              @endauth
+            </div>
+          </li>
+        </ul>
+     
+      </div>
+    </nav>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <nav class="sidebar sidebar-offcanvas" id="sidebar">
+      @section('sidebar')
+          @include('layouts.sidebar',['user' => Auth::User()])
+      @show
+      </nav>
+      <div class="main-panel">
+        <div class="content-wrapper">
+          @yield('content')
+        </div>
+        <footer class="footer">
+          <div class="container-fluid clearfix">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © {{date('Y')}}
+            <a href="http://smkn1seputihagung.sch.id/" target="_blank">SMKN 1 SEPA</a>. All rights reserved.</span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <script src="{{asset('admin/vendors/js/vendor.bundle.base.js')}}"></script>
+  <script src="{{asset('admin/vendors/js/vendor.bundle.addons.js')}}"></script>
+  <script src="{{asset('admin/js/off-canvas.js')}}"></script>
+  <script src="{{asset('admin/js/misc.js')}}"></script>
+  <script src="{{asset('admin/js/dashboard.js')}}"></script>
+  <script src="{{asset('admin/js/jquery.dataTables.min.js')}}"></script>
+  <script src="{{asset('admin/js/dataTables.bootstrap4.min.js')}}"></script>
+  
+  <script src="{{asset('admin/js/select2.min.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+
+  <script src="{{asset('ckeditor/ckeditor.js')}}"></script>
+  @stack('js')
+</body>
+
+</html>
+                         
+   
